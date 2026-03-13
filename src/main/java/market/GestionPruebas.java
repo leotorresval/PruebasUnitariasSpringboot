2690cf4a047e0dd4b9b24fb068dd6842f3e86a66
leotorresval92 <leotorresval92@gmail.com>
2026-03-13 17:15:07 -0500

    init file

--- src/main/java/market/GestionPruebas.java
+++ src/main/java/market/GestionPruebas.java
@@ -1,4 +1,36 @@
 package market;
 
-public class GestionPruebas {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class GestionPruebas extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPruebas.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GestionPruebas.class);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+            }
+
+        };
+    }
 }
