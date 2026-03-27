cabd04b7e4b1eafc1f7e0ccaf9c8ddc488102c83
leotorresval92 <leotorresval92@gmail.com>
2026-03-27 17:54:55 -0500

    Pruebas integracion

--- src/test/java/market/rest/ProductRestIntegrationTest.java
+++ src/test/java/market/rest/ProductRestIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
